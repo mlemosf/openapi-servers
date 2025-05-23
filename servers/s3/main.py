d1152bdfc74523c0375f1d621825f77072889d03
Matheus Fernandes <matheuslemosf@protonmail.com>
2025-05-23 15:33:16 -0300

    added: Estatísticas de objeto
    Adicionada nova rota para retorno de estatísticas de objeto.

--- servers/s3/main.py
+++ servers/s3/main.py
@@ -5,6 +5,7 @@ from os import getenv
 import pandas as pd
 import boto3
 import io
+import logging
 
 #-----------------
 # Environment variables
@@ -25,8 +26,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST"],
-    allow_headers=["*"],
+    allow_methods=["GET", "POST", "OPTIONS"],
+    allow_headers=["*"]
 )
 
 # -------------------------------
@@ -59,11 +60,11 @@ def analyze_s3_object(data: S3ObjectInput):
         "s3",
         endpoint_url=AWS_ENDPOINT_URL,
         aws_access_key_id=AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
         aws_session_token=None,
         config=boto3.session.Config(signature_version='s3v4'),
         region_name='us-east-1',
-        verify=False
+        verify=True
     ) 
 
     try:
@@ -79,7 +80,7 @@ def analyze_s3_object(data: S3ObjectInput):
         dataframe = object_to_dataframe(response['Body'], file_extension)
         return {
             "status": 200,
-            "records": dataframe
+            "records": dataframe.head(50).to_dict('records')
         }
     except Exception as e:
         return {
@@ -87,39 +88,55 @@ def analyze_s3_object(data: S3ObjectInput):
         }
 
 @app.post(
-    "/get_s3_object_metadata",
-    summary="Get metadata about a file in s3"
+    "/get_s3_object_stats",
+    summary="Get statistics about a S3 object."
 )
-def get_s3_object_metadata(data: S3ObjectInput):
+def get_s3_object_stats(data: S3ObjectInput):
     """
-    Return object metadata
+    Get statistics about a S3 object, including number of rows and columns and null value count and data type per column. 
+    File size, informations about connection and other S3 headers are also provided.
     """
     # Create an S3 client using the provided credentials
     s3_client = boto3.client(
         "s3",
         endpoint_url=AWS_ENDPOINT_URL,
         aws_access_key_id=AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
         aws_session_token=None,
         config=boto3.session.Config(signature_version='s3v4'),
         region_name='us-east-1',
-        verify=False
+        verify=True
     ) 
 
     try:
         # Check if the key has a valid extension (csv, xlsx, parquet)
-        tags = s3_client.get_object_tagging(Bucket=data.bucket_name, Key=data.object_key)['TagSet']
+        file_extension = get_file_extension(data.object_key)
+        if file_extension is None:
+            raise ValueError("The file does not have an allowed extension (csv, xlsx or parquet).")
+
+        # Check if the key has a valid extension (csv, xlsx, parquet)
+        tags = s3_client.get_object_tagging(Bucket=data.bucket_name, Key=data.object_key)
+
+        # Get the object from S3
+        response = s3_client.get_object(Bucket=data.bucket_name, Key=data.object_key)
+
+        # Convert the value to a pandas dataframe
+        dataframe = object_to_dataframe(response['Body'], file_extension)
+        
+        # Get statistics about the data
+        stats = get_object_statistics(dataframe)
         return {
             "status": 200,
-            "metadata": {
-                "tags": tags
-            }
-            
+            "metadata": tags,
+            "statistics": stats
         }
     except Exception as e:
         return {
+            "status": 400,
             "message": f"The file could not be processed with error: {e}"
         }
+
+
 # ------------------------------------------------
 # Utilitary functions
 # ------------------------------------------------
@@ -142,9 +159,31 @@ def object_to_dataframe(data, file_extension):
     if file_extension == "csv":
         # Read the CSV data into a pandas DataFrame
         df = pd.read_csv(io.BytesIO(data.read()), sep=';')
-        return df.head(50).to_dict('records')
+        return df
     elif file_extension == "parquet":
         df = pd.read_parquet(io.BytesIO(data.read()))
-        return df.head(50).to_dict('records')
+        return df
     else:
         raise ValueError(f"Unsupported file extension '{file_extension}'")
+
+def get_object_statistics(data : pd.DataFrame):
+    """
+    Get the statistics of an S3 object.
+    """
+
+    columns = list(data.columns)
+    column_types = {}
+    stats = {
+        "rows": {
+            "count": len(data.index),
+        },
+        "columns": [
+            {
+                "name": col,
+                "type": str(data[col].dtype),
+                "null_value_count": int(data[col].isnull().sum())
+            } for col in columns
+        ]
+    }
+    return stats
+
